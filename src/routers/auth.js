49b542aa87f0f50b2cd518011210857548ebe1e3
besit06 <behemoth2054@gmail.com>
2025-02-15 17:44:48 +0100

    hw6-email-and-images

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -13,9 +13,9 @@ router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserContro
 router.post('/logout', ctrlWrapper(logoutUserController));
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
-router.post('/request-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
+router.post('/send-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
 
-router.post('/reset-password', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
+router.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
 
 
 export default router;
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,7 +4,7 @@ export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
     email: Joi.string(),
-    isFavourite: Joi.boolean(),
+    isFavourite: Joi.string().valid('true', 'false'),
     contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
@@ -12,7 +12,7 @@ export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
     phoneNumber: Joi.string().min(3).max(20),
     email: Joi.string(),
-    isFavourite: Joi.boolean(),
+    isFavourite: Joi.string().valid('true', 'false'),
     contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
